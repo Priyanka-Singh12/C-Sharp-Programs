5a9edca51df0b2a258c1aea681c09aff804d0168
Priyanka Singh <87170979+Priyanka-Singh12@users.noreply.github.com>
2021-07-13 00:52:57 +0530

    Add files via upload

---
+++ BitArrayEg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Practical.Non_Generic_Collection
+{
+    class BitArrayEg
+    {
+        //BitArray are of Fixed length.
+
+        static void Main()
+        {
+            BitArray obj = new BitArray(3);
+
+            obj[0] = false;
+            obj[1] = true;
+            obj[2] = false;
+
+
+            Console.WriteLine(obj.Get(1));
+        }
+
+    }
+}
